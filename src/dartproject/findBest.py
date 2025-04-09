69d1b6df10f5610fb80d14ff5f5b92326abbfa75
acdcjakob <acdcjakob1@gmail.com>
2025-04-09 15:05:54 +0200

    created correct findBest function

--- src/dartproject/findBest.py
+++ src/dartproject/findBest.py
@@ -1,21 +1,37 @@
 import scipy as sp
 import numpy as np
-from .scoring import score_avg_integrand
-from .utils import monte_carlo
+rng = np.random.default_rng()
+
+# from .scoring import score_avg_integrand
+# from .utils import monte_carlo_uniform
+
+from .scoring import calc_score
+from .utils import monte_carlo_general
 
 # def score_avg(t_x: float, t_y: float, sigma: np.ndarray) -> float:
 #     range = [(t_x-3*np.sqrt(sigma[0]), t_x+3*np.sqrt(sigma[0])), (t_y-3*np.sqrt(sigma[1]), t_y+3*np.sqrt(sigma[1]))]
 #     I = sp.integrate.dblquad(score_avg_integrand,*range[0],*range[1],args=(t_x,t_y,sigma))
 #     return I
 
-def score_avg(t_x: float, t_y: float, sigma: np.ndarray) -> float:
+# def score_avg(t_x: float, t_y: float, sigma: np.ndarray) -> float:
 
-    passToMonteCarlo = lambda h_x, h_y: score_avg_integrand(h_x,h_y,t_x,t_y,sigma)
+#     passToMonteCarlo = lambda h_x, h_y: score_avg_integrand(h_x,h_y,t_x,t_y,sigma)
 
-    lim_mult = 6
-    sample_number = 1000000
+#     lim_mult = 6
+#     sample_number = 1000000
 
-    limits = [[t_x - lim_mult*sigma[0], t_x + lim_mult*sigma[0]],
-              [t_y - lim_mult*sigma[1], t_y + lim_mult*sigma[1]]]
+#     limits = [[t_x - lim_mult*sigma[0], t_x + lim_mult*sigma[0]],
+#               [t_y - lim_mult*sigma[1], t_y + lim_mult*sigma[1]]]
 
-    return monte_carlo(passToMonteCarlo,limits,sample_number)[0]
+#     return monte_carlo_uniform(passToMonteCarlo,limits,sample_number)[0]
+
+def score_avg(t_x: float, t_y: float, sigma: list) -> float:
+    N = 1_000 # sampling number
+    samples_x = rng.normal(t_x,sigma[0],(N,1))
+    samples_y = rng.normal(t_y,sigma[1],(N,2))
+    samples = np.hstack([samples_x,samples_y])
+
+    result = monte_carlo_general(calc_score,samples)
+
+    return result
+
--- tests/test_findBest.py
+++ tests/test_findBest.py
@@ -4,42 +4,10 @@ import scipy as sp
 
 from dartproject.findBest import score_avg
 
-# def test_score_avg():
-#     return score_avg(0,70,[1,1])
+def test_score_avg_gives60():
+    assert abs(score_avg(0,103,[.5,.5])-60)<0.1
 
-# def test_mapping():
-#     N = 100
-#     x = np.linspace(-170,170,N)
-#     y = np.linspace(-170,170,N)
-    
-#     res = np.zeros((N,N))
-#     sigma = [1,1]
-#     for yi in range(N):
-#         print(yi)
-#         for xi in range(N):
-#             res[xi,yi] =  score_avg(x[xi],y[yi],sigma)
-
-#     # plt.scatter(x,y,c=res)
-#     plt.pcolormesh(res)
-#     plt.show()
-
-# # print(test_score_avg())
-
-# test_mapping()
-
-def test_score_avg_givesFloat():
-    A = [[0,0],[0,102],[102,0],[60,60],[0,-102]]
-    sigma = [0.1,0.1]
-    for a in A:
-        print(f'aim at {a}:',f'{score_avg(*a,sigma):.2f}')
-        assert abs(score_avg(*a,sigma)) >= 0
-
-def test_score_avg_reproducibility60():
-    a = [0,102]
-    sigma = [0.01,0.01]
-
-    for i in range(5):
-        print(f'aim at {a}:',f'{score_avg(*a,sigma):.2f}')
-        assert abs(score_avg(*a,sigma)-60) <= 2
-
-test_score_avg_reproducibility60()
+def test_score_avg_gives0():
+    assert abs(score_avg(0,200,[.5,.5]))<0.1
+    assert abs(score_avg(-200,-200,[.5,.5]))<0.1
+    assert abs(score_avg(-200,200,[.5,.5]))<0.1
